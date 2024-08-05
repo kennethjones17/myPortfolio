afb338876589d8f5bd2581be8e67974d0a14d203
Kenneth <kennethjones1737@gmail.com>
2024-08-05 00:11:22 -0400

    small bugs

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = () => {
       <div className='flex justify-center relative my-20 z-10'>
         <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
           <h1 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>
-            Hello, I'm Kenneth Jones
+            Hello, I&apos;m Kenneth Jones
           </h1>
 
           <TextGenerateEffect
